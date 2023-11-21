f88025d82c770764a039ffa18ebc9daa852da333
Luskete <92869197+Luskete@users.noreply.github.com>
2023-11-21 09:24:14 -0300

    :alien:

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
-    public List<Card> allCards; // Coloque todas as cartas disponíveis aqui
+    public List<Card> allCards = new List<Card>(); // Coloque todas as cartas disponíveis aqui
     public List<Card> playerHand; // Mão do jogador
     public int startingHandSize = 6; // Tamanho inicial da mão do jogador
 
     void Start()
     {
+        // Certifique-se de que allCards está inicializado
+        if (allCards == null)
+        {
+            Debug.LogError("A lista de cartas não foi inicializada.");
+            return;
+        }
+
         // Verifique se há cartas suficientes para distribuir
         if (allCards.Count < startingHandSize)
         {
@@ -16,19 +23,13 @@ public class CardManager : MonoBehaviour
             return;
         }
 
-        // Distribua cartas aleatórias para a mão do jogador
-        for (int i = 0; i < startingHandSize; i++)
-        {
-            int randomIndex = Random.Range(0, allCards.Count);
-            Card randomCard = allCards[randomIndex];
-            playerHand.Add(randomCard);
-            allCards.RemoveAt(randomIndex);
+            // Distribua cartas aleatórias para a mão do jogador
+            for (int i = 0; i < startingHandSize; i++)
+            {
+                int randomIndex = Random.Range(0, allCards.Count);
+                Card randomCard = allCards[randomIndex];
+                playerHand.Add(randomCard);
+                allCards.RemoveAt(randomIndex);
+            }
         }
-    }
-        void LoadAllCards()
-    {
-        // Carregue todos os Scriptable Objects de Cartas da pasta Resources/Cards
-        Card[] cards = Recursos.LoadAll<Card>("Cartas");
-        allCards.AddRange(cards);
-    }
 }
